4f5055dd6b94d6b8dc219cead2d99c942e072de9
Fodé Guirassy <fode.guirassy@ekino.com>
2018-08-20 17:48:41 +0200

    add style to ActionBar

--- app/src/main/java/com/fodeguirassy/littleafrica/MainActivity.kt
+++ app/src/main/java/com/fodeguirassy/littleafrica/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.fodeguirassy.littleafrica
 
-import android.arch.lifecycle.LifecycleOwner
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(), LifecycleOwner {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
